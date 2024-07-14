da9efb117fd38c41acac3a90ccfffb7d33b496e8
sba <andreabsferrazza@gmail.com>
2024-07-14 11:08:03 +0200

    Array unique

--- src/create_dictionary.c
+++ src/create_dictionary.c
@@ -1,56 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-int create_dictionary(){
-	FILE *file = fopen("./test/input", "r");
-	char *code;
-	size_t n = 0;
+struct dictionary_info{
+	int max_word_length;
+	int total_words;
+};
+
+struct dictionary_info scan_words(char * filename){
+	struct dictionary_info di;
 	int c;
-	char space = ' ';
+	int n=0;
+	FILE *file = fopen(filename, "r");
+	int max_word_length = 0;
+	int total_words = 0;
+	if (file == NULL){
+		di.max_word_length = max_word_length;
+		di.total_words= total_words;
+		return di; 
+	}
+	do{
+		c=fgetc(file);
+		// se abbiamo già incontrato un carattere che non sia EOF o uno spazio
+		// AND il carattere corrente è EOF o uno spazio
+		if(n>0 && (c==' ' || c==EOF)){
+			// abbiamo quindi trovato una parola
+			total_words++;
+			// Calcolo massima lunghezza di una parola
+			if(n>max_word_length){
+				max_word_length=n;
+			}
+			n=0;
+			continue;
+		}
+		// se il carattere corrente c è tra ?!. 
+		if(c=='?' || c=='!' || c=='.'){
+			// se n>0 abbiamo già trovato un carattere che fa parte di una parola
+			// dobbiamo quindi registrare la parola in "sospeso" e poi memorizzare c
+			if(n>0){
+				total_words++;
+				if(n>max_word_length){
+					max_word_length=n;
+				}
+			}
+			total_words++;
+			if(n>max_word_length){
+				max_word_length=n;
+			}
+			n=0;
+			continue;
+		}
+		// se il carattere corrente c è tra quelli che possono comporre una parola
+		if( (c>='a' && c<='z') || (c>='A' && c<='Z') || c=='\''){
+			// contiamo con il contatore n il numero di lettere della parola che stiamo leggendo
+			n++;
+		}
+	}while (c != EOF);
+	di.max_word_length = max_word_length;
+	di.total_words= total_words;
+	return di;
+}
 
-	if (file == NULL)
-		return 1; 
-
-	int sizecode = 1000;
-	// TODO gestire varie grandezze files
-	code = malloc(sizecode);
-	char words_array[sizecode][sizecode];
-	// char next_word_array[sizecode][sizecode][sizecode];
-	int words = 0;
-
+int create_dictionary(char * filename){
 	/*	Each phrase is terminated by . ? ! or EOF 
 	 *	. ? and ! are words
 	 *	every other character sequence between spaces and/or . ? ! is a word
 	 *	the last word of the file has the first as "next"
+	 *	all other characters are ignored except for a-zA-Z'
 	 * */
-	do{
-		c=fgetc(file);
-		if(n>0 && (c==space || c==EOF)){
-			for(int i=0;i<n;i++){
-				words_array[words][i] = code[i];
-			}
-			words++;
-			n=0;
-			continue;
-		}
-		if(c=='?' || c=='!' || c=='.'){
-			if(n>0){
-				for(int i=0;i<n;i++){
-					words_array[words][i] = code[i];
-				}
-				words++;
-			}
-			words_array[words][0] = c;
-			words++;
-			n=0;
-			continue;
-		}
-		if( (c>='a' && c<='z') || (c>='A' && c<='Z') || c=='\''){
-			code[n++] = (char) c;
-		}
-	}while (c != EOF);
+	// TODO può contenere numeri?
+	// Determines max_word_length and total_words
 
-	code[n] = '\0';
-	for(int i=0;i<words;i++){
-		printf("%s\n",words_array[i]);
+	struct dictionary_info di;
+	int c;
+	di = scan_words(filename);
+
+	if(di.total_words>0 && di.max_word_length>0){
+		int total_words = di.total_words;
+		int max_word_length = di.max_word_length;
+		printf("Found %d words | %d maxlength of a word.\n",total_words,max_word_length);
+		// char* current_string;
+		// current_string = malloc( max_word_length * sizeof(char) );
+		// char* words_array[total_words+1];
+		char words_array[total_words][max_word_length+1];
+		char words_array_unique[total_words][max_word_length+1];
+		int words = 0, n=0;
+		// printf("%d\n",words);
+		FILE *file_catcher = fopen(filename, "r");
+		do{
+			c=fgetc(file_catcher);
+			if(n>0 && (c==' ' || c==EOF)){
+				/* for(int i=0;i<n;i++){
+					printf("%c\n",current_string[i]);
+					words_array[words][i] = current_string[i];
+					printf("%c\n",words_array[words][i]);
+					printf("%s\n",words_array[words]);
+				} */
+				words_array[words][n++] = '\0';
+				words++;
+				n=0;
+				continue;
+			}
+			if(c=='?' || c=='!' || c=='.'){
+				if(n>0){
+					/* for(int i=0;i<n;i++){
+						words_array[words][i] = current_string[i];
+					} */
+					words_array[words][n++] = '\0';
+					words++;
+					n=0;
+				}
+				words_array[words][n++] = c;
+				words_array[words][n++] = '\0';
+				words++;
+				n=0;
+				continue;
+			}
+			if( (c>='a' && c<='z') || (c>='A' && c<='Z') || c=='\''){
+				words_array[words][n++] = (char) c;
+			}
+		}while (c != EOF);
+
+		// words_array
+		int z,check;
+		int u=0;
+		for(int i=0;i<words;i++){
+			// printf("%s - %d\n",words_array[i],i);
+			check=0;
+			for(int j=0;j<i;j++){
+				z=0;
+				while(words_array[i][z]!='\0' && words_array[j][z]!='\0'){
+					if(words_array[i][z]!=words_array[j][z]){
+						check=0;
+						break;
+					}
+					// printf("%d : %c - %d : %c\n",i,words_array[i][z],j,words_array[j][z]);
+					check++;
+					z++;
+				}
+				if(check>0){
+					break;
+				}
+			}
+			if(check==0){
+				z=0;
+				u++;
+				do{
+					words_array_unique[u][z]=words_array[i][z];
+					z++;
+				}while(words_array[i][z]!='\0');
+				words_array_unique[u][z++]='\0';
+				printf("%s\n",words_array_unique[u]);
+			}
+		}
+		fclose(file_catcher);
 	}
 	return 0;
 }
--- src/nwg.c
+++ src/nwg.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "../lib/argtable3.h"
 #include "create_dictionary.c"
 
@@ -8,52 +7,52 @@ struct arg_file *o, *file;
 struct arg_end *end;
 
 int main(int argc, char *argv[]){
-    /* the global arg_xxx structs are initialised within the argtable */
-    void *argtable[] = {
-        help    		= arg_litn("h", "help", 0, 1, "display this help and exit"),
-	create_dictionary_opt	= arg_litn("c", "create_dictionary", 0, 1,"Create dictionary (Requires option --file)"),
-        file    		= arg_filen("f", "file", "<file>", 0, 1, "Input file (1)"),
-        end     		= arg_end(20),
-    };
-    
-    int exitcode = 0;
-    char progname[] = "next-word-gen";
-    
-    int nerrors;
-    nerrors = arg_parse(argc,argv,argtable);
+	/* the global arg_xxx structs are initialised within the argtable */
+	void *argtable[] = {
+		help    		= arg_litn("h", "help", 0, 1, "display this help and exit"),
+		create_dictionary_opt	= arg_litn("c", "create_dictionary", 0, 1,"Create dictionary (Requires option --file)"),
+		file    		= arg_filen("f", "file", "<file>", 0, 1, "Input file (1)"),
+		end     		= arg_end(20),
+	};
 
-    /* special case: '--help' takes precedence over error reporting */
-    if (help->count > 0){
-        printf("Usage: %s", progname);
-        arg_print_syntax(stdout, argtable, "\n");
-        printf("Options for next-word-gen:\n\n");
-        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
-        exitcode = 0;
-        goto exit;
-    }
+	int exitcode = 0;
+	char progname[] = "next-word-gen";
 
-    /* If the parser returned any errors then display them and exit */
-    if (nerrors > 0){
-        /* Display the error details contained in the arg_end struct.*/
-        arg_print_errors(stdout, end, progname);
-        printf("Try '%s --help' for more information.\n", progname);
-        exitcode = 1;
-        goto exit;
-    }
+	int nerrors;
+	nerrors = arg_parse(argc,argv,argtable);
 
-    if (create_dictionary_opt->count > 0 && file->count==1){
-	exitcode = create_dictionary();	
-	// TODO gestione passaggio nomefile
-    }else{
-	arg_print_errors(stdout, end, progname);
-	printf("Try '%s --help' for more information.\n", progname);
-	exitcode = 2;
-	goto exit;
-    }
+	/* special case: '--help' takes precedence over error reporting */
+	if (help->count > 0){
+		printf("Usage: %s", progname);
+		arg_print_syntax(stdout, argtable, "\n");
+		printf("Options for next-word-gen:\n\n");
+		arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+		exitcode = 0;
+		goto exit;
+	}
+
+	/* If the parser returned any errors then display them and exit */
+	if (nerrors > 0){
+		/* Display the error details contained in the arg_end struct.*/
+		arg_print_errors(stdout, end, progname);
+		printf("Try '%s --help' for more information.\n", progname);
+		exitcode = 1;
+		goto exit;
+	}
+
+	if (create_dictionary_opt->count > 0 && file->count==1){
+		exitcode = create_dictionary("./test/input");	
+		// TODO gestione passaggio nomefile
+	}else{
+		arg_print_errors(stdout, end, progname);
+		printf("Try '%s --help' for more information.\n", progname);
+		exitcode = 2;
+		goto exit;
+	}
 
 exit:
-    /* deallocate each non-null entry in argtable[] */
-    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
-    return exitcode;
+	/* deallocate each non-null entry in argtable[] */
+	arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+	return exitcode;
 }
 
